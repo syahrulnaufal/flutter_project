141100a6c7e7a397f21734ac45624eed2f4c77f9
naufal <syahrultsaqib@gmail.com>
2025-03-26 06:12:37 +0700

    update: darkmode, UI, data from spreadsheet

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2148320915"
+    serviceWorkerVersion: "617084266"
   }
 });
